b34c7a99a4fda5901599309f1fe37707c109ebb7
Mohana Bansal <mohanabansal10@gmail.com>
2020-05-02 21:34:19 -0400

    WIP - trying to restart game

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
 app.use(express.static(__dirname));
-app.get('/ping', function (req, res) {
- return res.send('pong');
-});
 app.use('*', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
